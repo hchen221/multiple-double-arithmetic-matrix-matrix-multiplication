93a8cef0dd94ae704a350f353d5e50f0bee9d5ec
hchen221 <104645476+hchen221@users.noreply.github.com>
2025-04-14 10:03:24 -0500

    Add files via upload

---
+++ C++/TCtest.cpp
@@ -0,0 +1,40 @@
+#include "TCflat.h"
+#include <iostream>
+#include <functional>
+
+using namespace std;
+
+int main() {
+    int p = 2;
+    int n = 16;
+    int nfrag = 4;
+    int expmin = 0;
+    int expmax = 0;
+    
+    vector<double> A = mat(n,p,expmin,expmax);
+    vector<double> B = mat(n,p,expmin,expmax);
+    /*
+    for (int i=0; i<n*n; i++) {
+        if (i%n==0) {
+            cout << endl;
+        }
+        cout << "(";
+        for (int j=0; j<p; j++) {
+            cout << A[i*n+j] << ",";
+        }
+        cout << ") ";
+    }
+
+    return 0;
+    */
+    vector<double> A8 = split4pd(A);
+    vector<double> B8 = split4pd(B);
+    
+    vector<double> C1 = zeros(n,4*p);
+    flatTCKernel(A8,B8,C1,n,4*p);
+    vector<double> C2 = zeros(n,4*p);
+    flatmatconv(A8,B8,C2,n,4*p);
+
+    cout << "C1 applies kernel directly\nC2 uses split matrices\nError of C1,C2 : " << max_err(C1,C2) << endl;
+    return 0;
+}
